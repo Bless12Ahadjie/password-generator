0cc4058d60b2e77810b36a6eb995bc9f6dcc21d0
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-05-22 16:49:51 +0000

    initializing develop branch

--- app/src/main/java/com/example/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.example.passwordgenerator
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
