ef09afc80ba7c421a440cc09f798372cd1b9b431
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-05-23 09:58:53 +0000

    created the view for the check password strength

--- app/src/main/java/com/example/passwordgenerator/MainActivity2.kt
+++ app/src/main/java/com/example/passwordgenerator/MainActivity2.kt
@@ -1,6 +1,10 @@
 package com.example.passwordgenerator
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,33 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navigateBtn = findViewById<Button>(R.id.myButton)
+
+        navigateBtn.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+
+        }
+        val strengthIndicatorContainer = findViewById<LinearLayout>(R.id.strength_indicator_container)
+        val layoutInflater = LayoutInflater.from(this)
+
+
+        for (i in 1..5) {
+            val strengthIndicatorView = layoutInflater.inflate(R.layout.dot, null)
+            strengthIndicatorContainer.addView(strengthIndicatorView)
+        }
+
+
+
+
+
+
+
     }
+
+
+
+
 }
